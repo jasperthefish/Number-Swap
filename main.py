35b5630ed2f8c426cd5ccdb1aea0099802cdf143
jasperthefish <vrithikavaithi@gmail.com>
2026-02-15 12:21:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 # Take two number from the user, store them in variables x and y respectively.
 # Swap the values present inside x and y and display the results
 
-x = float(input("Enter a number: "))
-y = float(input("Enter another number: "))
+# x = float(input("Enter a number: "))
+# y = float(input("Enter another number: "))
 
-num1 = x
-x = y
-y = num1
+# num1 = x
+# x = y
+# y = num1
 
-print(x)
-print(y)
+# print(x)
+# print(y)
+
+
+a = 5
+b = 9
+c = 1
+
+
+a, b, c = c, a, b
+print(a, b, c)
